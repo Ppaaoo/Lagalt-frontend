1551f6194ac0d578b4771531cc789128be7ca1e0
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-03-06 16:23:19 +0100

    basic routing added

--- lagalt-frontend/src/app/app-routing.module.ts
+++ lagalt-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+import { ProfilePage } from './pages/profile/profile.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginPage
+  },
+  {
+    path: "profile",
+    component: ProfilePage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
