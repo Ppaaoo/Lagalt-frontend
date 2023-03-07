2248ab2c69a0cd5e85fdc575aaf1f9a527776150
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-03-07 09:44:02 +0100

    login functionality implemented, not tested

--- lagalt-frontend/src/app/services/login.service.ts
+++ lagalt-frontend/src/app/services/login.service.ts
@@ -1,17 +1,47 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
+
+const { apiUsers, apiKey } = environment;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
 
   //Login
+  public login(username: string): Observable<User> {
+      return this.checkUsername(username)
+        .pipe(
+          switchMap((userResponse: User | undefined) => {
+              if(userResponse == undefined) {
+                return this.createUser(username);
+              }
+              return of(userResponse);
+          })
+        )
+  }
 
-  //Check if user exists
+  private checkUsername(username: string): Observable<User | undefined> {
+    return this.http.get<User[]>(`${apiUsers}?username=${username}`)
+    .pipe(
+      map((response: User[]) => response.pop())
+    )
+  }
 
-  //If !user - Create user
+  private createUser(username: string): Observable<User> {
+    const user = {
+      username,
+    };
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "x-api-ker": apiKey
+    });
 
-  //If user || user created- continue (Store user)
+    return this.http.post<User>(apiUsers, user, { headers })
+  }
 }
--- lagalt-frontend/src/environments/environment.ts
+++ lagalt-frontend/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     apiUsers: "",
     apiProjects: "",
+    apiKey: "",
 };
