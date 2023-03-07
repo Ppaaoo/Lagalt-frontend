a38af9157bcda648241f260b5986ccc0ac706240
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-03-07 09:11:51 +0100

    HttpClientModule added

--- lagalt-frontend/src/app/app.module.ts
+++ lagalt-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
@@ -8,15 +9,16 @@ import { ProfilePage } from './pages/profile/profile.page';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ //Components
     AppComponent,
     LoginPage,
     ProfilePage,
     LoginFormComponent
   ],
-  imports: [
+  imports: [ //Modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
