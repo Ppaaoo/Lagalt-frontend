6e0e5415b663549ff14c91eabeb57c467e596074
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-03-07 09:03:17 +0100

    Environment files added

--- lagalt-frontend/src/app/services/login.service.ts
+++ lagalt-frontend/src/app/services/login.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   constructor() { }
+
+  //Login
+
+  //Check if user exists
+
+  //If !user - Create user
+  
+  //If user || user created- continue (Store user)
 }
